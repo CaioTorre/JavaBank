02da8391266874f8cbfeace14a6a107727b6589f
Caio Torre <caiosanches@live.com>
2018-10-30 17:30:27 -0300

    Code cleanup
    Most functions now private + comment cleanup + clarifications

--- Hashing.java
+++ Hashing.java
@@ -2,28 +2,46 @@ import java.math.BigInteger;
 import java.util.Arrays;
 
 public abstract class Hashing {	
-	public static byte[] catArray(byte[] a, byte[] b) {
+	private static byte[] catArray(byte[] a, byte[] b) { //Joins two byte arrays into one
 		byte[] c = new byte[a.length + b.length];
 		System.arraycopy(a, 0, c, 0, a.length);
 		System.arraycopy(b, 0, c, a.length, b.length);
 		return c;
 	}
-	public static byte[] intToByteArray( final int i ) {
+	private static byte[] intToByteArray( final int i ) { //Converts an int into a byte array
 		BigInteger bigInt = BigInteger.valueOf(i);   
 		return bigInt.toByteArray();
 	}
 	
-	public static int getVal(byte[] arr) {
+	private static int getVal(byte[] arr) { //Gets the integer value of four bytes (from array)
 		return arr[3] + 256 * arr[2] + 256 * 256 * arr[1] + 256 * 256 * 256 * arr[0];
 	}
 	
-	public static int leftrotate(int arr, int k) { return (arr << k) | (arr >> (32 - k)); }
+	private static int leftrotate(int arr, int k) { return (arr << k) | (arr >> (32 - k)); } //Bitwise left rotation
 	
+	private static String intArrToHex(int[] arr) { //Converts integer array to hexadec string
+		StringBuilder builder = new StringBuilder(arr.length * 8);
+		for (int b : arr) {
+			builder.append(byteToUnsignedHex(b));
+		}
+		return builder.toString();
+	}
+	
+	private static String byteToUnsignedHex(int i) { //Converts integer to hexadec string
+		String hex = Integer.toHexString(i);
+		while(hex.length() < 8){
+			hex = "0" + hex; 
+		}
+		return hex;
+	}
+  
 	public static String hash(String in) {
 		int i;
 		int[] r = new int[64];
 		int[] k = new int[64];
+		//=================================== INIT HASHING FUNCTION VARIABLES ===================================
 		for (i = 0; i < 64; i++) { k[i] = (int)Math.floor(Math.abs(Math.sin(i + 1)) * Math.pow(2, 32)); }
+		
 		int h0 = 0x67452301;
 		int h1 = 0xEFCDAB89;
 		int h2 = 0x98BADCFE;
@@ -46,7 +64,7 @@ public abstract class Hashing {
 			}
 		}
 		byte[] msg = in.getBytes();
-		while ((msg.length * 8) % 512 > 0) { msg = catArray(msg, new byte[] {0x00}); }
+		while ((msg.length * 8) % 512 > 0) { msg = catArray(msg, new byte[] {0x00}); } //Should be % 512 < 448, then append original size as integer
 		
 		int a;
 		int b;
@@ -57,10 +75,11 @@ public abstract class Hashing {
 		byte[] current = new byte[64];
 		byte[][] words = new byte[16][4];
 		
+		//=================================== BEGIN HASHING FUNCTION ===================================
 		while (msg.length > 0) {
-			current = Arrays.copyOfRange(msg, 0, 64);
-			msg = Arrays.copyOfRange(msg, 64, msg.length);
-			for (j = 0; j < 16; j++) { words[j] = Arrays.copyOfRange(current, 4*j, 4*j + 4); }
+			current = Arrays.copyOfRange(msg, 0, 64);		//Pop a new block
+			msg = Arrays.copyOfRange(msg, 64, msg.length);	//from message
+			for (j = 0; j < 16; j++) { words[j] = Arrays.copyOfRange(current, 4*j, 4*j + 4); } //Split current block into 16 words
 			a = h0;
 			b = h1;
 			c = h2;
@@ -91,22 +110,6 @@ public abstract class Hashing {
 			h3 = h3 + d;
 		}
 		
-		return intArrToHex(new int[] {h0, h1, h2, h3});
+		return intArrToHex(new int[] {h0, h1, h2, h3}); //Join the 4 ints into a big hexadec string
 	}
-	
-	public static String intArrToHex(int[] arr) {
-		StringBuilder builder = new StringBuilder(arr.length * 8);
-		for (int b : arr) {
-			builder.append(byteToUnsignedHex(b));
-		}
-		return builder.toString();
-	}
-	
-	public static String byteToUnsignedHex(int i) {
-    String hex = Integer.toHexString(i);
-    while(hex.length() < 8){
-      hex = "0" + hex; 
-    }
-    return hex;
-  }
 }
